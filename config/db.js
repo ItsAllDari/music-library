02a98fe94a3308c9a4c265248fabf01170591e1a
ItsAllDari <itsalldari@gmail.com>
2020-06-30 14:47:42 -0400

    Updated base name
    Added music library name to db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'music-library'
 
 // create the mongodb uri for development and test
 const database = {

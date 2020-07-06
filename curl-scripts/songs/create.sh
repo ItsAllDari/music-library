2ab7dde2fd3c8e2e63ecfa0518c5a8031c8721c7
ItsAllDari <itsalldari@gmail.com>
2020-07-06 08:57:07 -0400

    Corrected the song curl scripts
    Added CRUD

---
+++ curl-scripts/songs/create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/songs"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "song": {
+      "title": "'"${TITLE}"'",
+      "album": "'"${ALBUM}"'",
+      "artist": "'"${ARTIST}"'"
+    }
+  }'
+
+echo
